03a39047e01d9a3f8b2016dc464cb78bb124c44d
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-18 09:39:14 +0100

    correction task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,20 +6,20 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, small, tmp;
+	size_t i, j, min, tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		small = i;
+		min = i;
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[small])
+			if (array[j] < array[min])
 			{
-				small = j;
+				min = j;
 			}
-			tmp = array[small];
-			array[small] = array[i];
+			tmp = array[min];
+			array[min] = array[i];
 			array[i] = tmp;
 		}
 		print_array(array, size);
